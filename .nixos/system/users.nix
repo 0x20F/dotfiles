d47eb5c21d98e660463bfe41f46c2dbe6790442f
0x20F <alexhexan22@gmail.com>
2021-06-01 22:01:43 +0200

    Install vim-plug for just one user

--- .nixos/system/users.nix
+++ .nixos/system/users.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 
+
 {
 	# Define user accounts. Don't forget to set a password with 'passwd'.
 	users.users.snowwhite = {
@@ -8,4 +9,14 @@
 		extraGroups = [ "wheel" ]; # Enable 'sudo' for the user.
 	};
 
+
+	system.userActivationScripts.snowwhite-vim-plug.text = ''
+		mkdir -p /home/snowwhite/.vim/autoload
+		rm -rf /tmp/plug
+
+		${pkgs.git}/bin/git clone https://github.com/junegunn/vim-plug /tmp/plug
+
+		mv /tmp/plug/plug.vim /home/snowwhite/.vim/autoload
+
+	'';
 }
