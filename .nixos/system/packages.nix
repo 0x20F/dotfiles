1aba2ee801d170aa91eca7dfb262f3e868a6bbee
0x20F <alexhexan22@gmail.com>
2021-06-03 12:48:17 +0200

    Add proper fonts

--- .nixos/system/packages.nix
+++ .nixos/system/packages.nix
@@ -1,40 +1,56 @@
 { config, pkgs, ... }:
 
 let
-	# Personal scripts that need to be global 
-	scripts = pkgs.callPackage ./scripts.nix { };
+  # Personal scripts that need to be global 
+  scripts = pkgs.callPackage ./scripts.nix { };
 in
 {
-	nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-	environment.systemPackages = with pkgs; [
-		scripts.ix
+  environment.systemPackages = with pkgs; [
+    scripts.ix
 
-		# Internet
-		wget curl
+    # Internet
+	wget curl
 
-		# Shell replacements
-		exa bat ripgrep fzf
+	# Shell replacements
+	exa bat ripgrep fzf
 
-		# Shell tools
-		vim tmux git htop
-		lolcat figlet jq psmisc
-		python3 xorg.xev
+	# Shell tools
+	vim tmux git htop
+	lolcat figlet jq psmisc
+	python3 xorg.xev
 
-		# Shell
-		zsh oh-my-zsh
+	# Shell
+	zsh oh-my-zsh
 
-		# Libs
-		glib
+	# Libs
+	glib
 
-		# Graphical Interface
-		feh rofi alacritty dunst
-		polybar picom maim imagemagick
-		firefox-devedition-bin-unwrapped
-		# google-chrome - add later, it's 64bit, windows VM won't run that
-		vlc
-		xfce.thunar xfce.gvfs xfce.tumbler
-		# obsidian - add later, it's 64bit, windows VM won't run that
-		i3lock-color
-	];
+	# Graphical Interface
+	feh rofi alacritty dunst
+	polybar picom maim imagemagick
+	firefox-devedition-bin-unwrapped
+	# google-chrome - add later, it's 64bit, windows VM won't run that
+	vlc
+	xfce.thunar xfce.gvfs xfce.tumbler
+	# obsidian - add later, it's 64bit, windows VM won't run that
+	i3lock-color
+  ];
+
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+
+    fonts = with pkgs; [
+      (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+
+      ubuntu_font_family
+      source-code-pro
+      source-sans-pro
+      dejavu_fonts
+      font-awesome-ttf
+      siji
+    ];
+  };
 }
