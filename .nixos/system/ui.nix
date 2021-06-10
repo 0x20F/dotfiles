c0e71d49053ba236ddef0671f3e7de9a9bcb2d03
0x20F <0x20fa@gmail.com>
2021-06-10 18:29:00 +0200

    Update settings to match 4K screen with high DPI

--- .nixos/system/ui.nix
+++ .nixos/system/ui.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
 {
+    environment.variables = {
+      GDK_SCALE = "1";
+      GDK_DPI_SCALE = "0.7";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    };
+
 	# Enable the X11 windowing system
 	services.xserver = {
 		enable = true;
@@ -9,19 +15,29 @@
 		layout = "se";		
 
 		autoRepeatDelay = 200;
-			autoRepeatInterval = 25;
+		autoRepeatInterval = 25;
 
 		desktopManager = {
 			xterm.enable = false;
-		};
+        };
 
+        displayManager = {
+          defaultSession = "none+bspwm";
 
-		displayManager = {
-			defaultSession = "none+bspwm";
-		};
-
+          lightdm.greeters.gtk.cursorTheme = {
+            name = "Vanilla-DMZ";
+            package = pkgs.vanilla-dmz;
+            size = 64;
+          };
+        };
 
 		windowManager.bspwm.enable = true;
+
+		libinput.enable = true;
+
+        dpi = 290;
+
+		#videoDrivers = [ "nvidia" ];
 	};
 
 
