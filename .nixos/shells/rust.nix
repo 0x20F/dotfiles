d5e36afe59d34f556daa22d083d4184733761833
0x20F <0x20fa@gmail.com>
2021-06-15 16:33:04 +0200

    Make it clear what environment we're in

--- .nixos/shells/rust.nix
+++ .nixos/shells/rust.nix
@@ -2,6 +2,13 @@ let
   pkgs = import <nixpkgs> {};
 
 in pkgs.mkShell {
+  shellHook = with pkgs; ''
+    ${pkgs.figlet}/bin/figlet -f slant "Rust"
+    date
+
+    export CURRENT_ENVIRONMENT="rust"
+  '';
+
   buildInputs = with pkgs; [
     cargo
     rustc
