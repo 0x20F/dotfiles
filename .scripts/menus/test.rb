00694a21d192256e041e602c6d0e979180f6ee12
0x20F <0x20fa@gmail.com>
2021-07-08 19:09:39 +0200

    Allow for easier import of custom modules

---
+++ .scripts/menus/test.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env
+$: << ENV['katana']
+
+
+require 'dev/wifi'
+require 'etc/menu'
+
+
--- .system/katana/utils.rb
+++ .system/katana/utils.rb
@@ -1,5 +1,7 @@
 # Aliases for ease of use
 HOME = ENV['HOME']
+KATANA = ENV['katana']
+
 
 
 # Get the directory for the user
@@ -17,4 +19,20 @@ end
 def string(length)
   o = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
   (0..length).map { o[rand(o.length)] }.join
+end
+
+
+
+# Custom import method to allow
+# requiring anything within katana without
+# having to specify directories
+#
+def use(*modules)
+  Dir["#{KATANA}/**/*.rb"].each do |file|
+    name = File::basename(file, '.*')
+
+    if modules.include? name
+      require file
+    end
+  end
 end
